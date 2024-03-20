d59426161230684b33519699f47ea527c163724f
Jay Ohms <jay.ohms@gmail.com>
2024-03-20 07:42:09 -0400

    Local dev url naming tweak

--- demo/src/main/kotlin/dev/hotwire/demo/Urls.kt
+++ demo/src/main/kotlin/dev/hotwire/demo/Urls.kt
@@ -5,12 +5,12 @@ object Urls {
     private const val useLocalDev = false
 
     // Update with your local dev IP address
-    private const val localUrl = "http://192.168.1.1:45678"
+    private const val localDevUrl = "http://192.168.1.1:45678"
 
     // Remote demo server
     private const val remoteUrl = "https://turbo-native-demo.glitch.me"
 
-    val appUrl = if (useLocalDev) localUrl else remoteUrl
+    val appUrl = if (useLocalDev) localDevUrl else remoteUrl
     val homeUrl = appUrl
     val signInUrl = "$appUrl/signin"
     val numbersUrl = "$appUrl/numbers"
