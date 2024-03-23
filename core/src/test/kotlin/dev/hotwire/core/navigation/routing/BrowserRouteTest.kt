3b0b6686a5b9f74d0067c3e67501b83587c38827
Jay Ohms <jay.ohms@gmail.com>
2024-03-23 06:55:34 -0400

    Fix test imports

--- core/src/test/kotlin/dev/hotwire/core/navigation/routing/BrowserRouteTest.kt
+++ core/src/test/kotlin/dev/hotwire/core/navigation/routing/BrowserRouteTest.kt
@@ -1,6 +1,6 @@
 package dev.hotwire.core.navigation.routing
 
-import dev.hotwire.core.navigation.activities.SessionConfiguration
+import dev.hotwire.core.navigation.session.SessionConfiguration
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
