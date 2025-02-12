8b039dd97f468d1e033f3ea484170026dd13aaf7
Milan Barta <milan@basecamp.com>
2025-02-12 11:55:28 +0100

    Enable edge-to-edge

--- demo/src/main/kotlin/dev/hotwire/demo/main/MainActivity.kt
+++ demo/src/main/kotlin/dev/hotwire/demo/main/MainActivity.kt
@@ -1,6 +1,10 @@
 package dev.hotwire.demo.main
 
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import dev.hotwire.demo.R
 import dev.hotwire.demo.Urls
 import dev.hotwire.navigation.activities.HotwireActivity
@@ -8,8 +12,15 @@ import dev.hotwire.navigation.navigator.NavigatorConfiguration
 
 class MainActivity : HotwireActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root_view)) { v, insets ->
+            insets.getInsets(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.ime())
+                .apply { v.setPadding(left, top, right, bottom) }
+            insets
+        }
     }
 
     override fun navigatorConfigurations() = listOf(
