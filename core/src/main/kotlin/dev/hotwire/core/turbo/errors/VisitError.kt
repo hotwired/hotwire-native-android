90778668ef43a91be9561328253f2082a2efd846
Jay Ohms <jay@37signals.com>
2025-06-18 15:15:52 -0400

    Include the error description in the default error view

--- core/src/main/kotlin/dev/hotwire/core/turbo/errors/VisitError.kt
+++ core/src/main/kotlin/dev/hotwire/core/turbo/errors/VisitError.kt
@@ -3,4 +3,11 @@ package dev.hotwire.core.turbo.errors
 /**
  * Represents all possible errors received when attempting to load a page.
  */
-sealed interface VisitError
+sealed interface VisitError {
+    fun description() = when (this) {
+        is HttpError -> reasonPhrase
+        is LoadError -> description
+        is WebError -> description
+        is WebSslError -> description
+    }
+}
