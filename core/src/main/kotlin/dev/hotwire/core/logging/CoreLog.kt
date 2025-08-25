496377a7badacb9f6ee59383e8e9d9d1fe84feff
Milan Barta <milan@basecamp.com>
2025-08-25 11:28:39 +0200

    Use string interpolation for padded event logging

--- core/src/main/kotlin/dev/hotwire/core/logging/CoreLog.kt
+++ core/src/main/kotlin/dev/hotwire/core/logging/CoreLog.kt
@@ -13,18 +13,18 @@ internal class HotwireHttpLogger : HttpLoggingInterceptor.Logger {
 }
 
 internal fun logEvent(event: String, details: String = "") {
-    Hotwire.config.logger.d(DEFAULT_TAG, "$event ".padEnd(PAD_END_LENGTH, '.') + " [$details]")
+    Hotwire.config.logger.d(DEFAULT_TAG, "${"$event ".padEnd(PAD_END_LENGTH, '.')} [$details]")
 }
 
 internal fun logEvent(event: String, attributes: List<Pair<String, Any>>) {
     val description = attributes.joinToString(prefix = "[", postfix = "]", separator = ", ") {
         "${it.first}: ${it.second}"
     }
-    Hotwire.config.logger.d(DEFAULT_TAG, "$event ".padEnd(PAD_END_LENGTH, '.') + " $description")
+    Hotwire.config.logger.d(DEFAULT_TAG, "${"$event ".padEnd(PAD_END_LENGTH, '.')} $description")
 }
 
 internal fun logWarning(event: String, details: String) {
-    Hotwire.config.logger.w(DEFAULT_TAG, "$event ".padEnd(PAD_END_LENGTH, '.') + " [$details]")
+    Hotwire.config.logger.w(DEFAULT_TAG, "${"$event ".padEnd(PAD_END_LENGTH, '.')} [$details]")
 }
 
 internal fun logError(event: String, throwable: Throwable) {
--- navigation-fragments/src/main/java/dev/hotwire/navigation/logging/NavigationLog.kt
+++ navigation-fragments/src/main/java/dev/hotwire/navigation/logging/NavigationLog.kt
@@ -6,18 +6,18 @@ private const val DEFAULT_TAG = "Hotwire-Navigation"
 private const val PAD_END_LENGTH = 35
 
 internal fun logEvent(event: String, details: String = "") {
-    Hotwire.config.logger.d(DEFAULT_TAG, "$event ".padEnd(PAD_END_LENGTH, '.') + " [$details]")
+    Hotwire.config.logger.d(DEFAULT_TAG, "${"$event ".padEnd(PAD_END_LENGTH, '.')} [$details]")
 }
 
 internal fun logEvent(event: String, attributes: List<Pair<String, Any>>) {
     val description = attributes.joinToString(prefix = "[", postfix = "]", separator = ", ") {
         "${it.first}: ${it.second}"
     }
-    Hotwire.config.logger.d(DEFAULT_TAG, "$event ".padEnd(PAD_END_LENGTH, '.') + " $description")
+    Hotwire.config.logger.d(DEFAULT_TAG, "${"$event ".padEnd(PAD_END_LENGTH, '.')} $description")
 }
 
 internal fun logWarning(event: String, details: String) {
-    Hotwire.config.logger.w(DEFAULT_TAG, "$event ".padEnd(PAD_END_LENGTH, '.') + " [$details]")
+    Hotwire.config.logger.w(DEFAULT_TAG, "${"$event ".padEnd(PAD_END_LENGTH, '.')} [$details]")
 }
 
 internal fun logError(event: String, throwable: Throwable) {
