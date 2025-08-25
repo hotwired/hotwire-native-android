e78df3833c8a339bf0474860c469540caa377809
Milan Barta <milan@basecamp.com>
2025-08-25 11:25:28 +0200

    Add verbose and info log methods to HotwireLogger

--- core/src/main/kotlin/dev/hotwire/core/logging/DefaultHotwireLogger.kt
+++ core/src/main/kotlin/dev/hotwire/core/logging/DefaultHotwireLogger.kt
@@ -6,8 +6,14 @@ import dev.hotwire.core.BuildConfig
 internal object DefaultHotwireLogger : HotwireLogger {
     private val debugEnabled get() = BuildConfig.DEBUG
 
+    override fun v(tag: String, msg: String) =
+        log(Log.VERBOSE, tag, msg)
+
     override fun d(tag: String, msg: String) =
         log(Log.DEBUG, tag, msg)
+
+    override fun i(tag: String, msg: String) =
+        log(Log.INFO, tag, msg)
 
     override fun w(tag: String, msg: String) =
         log(Log.WARN, tag, msg)
@@ -18,6 +24,8 @@ internal object DefaultHotwireLogger : HotwireLogger {
     private fun log(logLevel: Int, tag: String, msg: String, throwable: Throwable? = null) {
         when (logLevel) {
             Log.DEBUG -> if (debugEnabled) Log.d(tag, msg)
+            Log.VERBOSE -> if (debugEnabled) Log.v(tag, msg)
+            Log.INFO -> if (debugEnabled) Log.i(tag, msg)
             Log.WARN -> Log.w(tag, msg)
             Log.ERROR -> Log.e(tag, msg, throwable)
         }
